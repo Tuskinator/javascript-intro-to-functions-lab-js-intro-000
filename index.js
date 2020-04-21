718c70dd9415708024ad63c1013d307fdea6ce09
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-04-21 14:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  string2 = string.toUpperCase()
-  console.log(string2)
+  console.log(string2.toUpperCase())
 }
 function logWhisper(string) {
-  string2 = string.toLowerCase()
-  console.log(string2)
+  console.log(string.toLowerCase())
 }
