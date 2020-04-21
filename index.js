ba9a673cea5c0a556446333087bf0f16fc5c831e
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-04-21 14:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase() === string
+  var uppercase = string.toUpperCase() === string
+  var love = "I love you, Grandma." === string
+  if (lowercase) {
+    return "I can't hear you"
+  }
+  else if (uppercase) {
+    return "YES INDEED!"
+  }
+  else if (love) {
+    return "I love you, too."
+  }
+}
